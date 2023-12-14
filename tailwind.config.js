3362c7ea7b77d6fa4e1d237ed61532dea5ab3819
artiecodes <bjarke.dehlie@gmail.com>
2023-12-14 14:13:12 +0100

    added colours to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      "dat-blue": "#465D98",
+      "dat-olive": "#ADC698",
+      "dat-red": "#EB5160",
+      "dat-black": "#071013",
+      "dat-white": "#F8F8FF",
+    },
+    extend: {
+      colors: {
+        "dat-blue": "#465D98",
+        "dat-olive": "#ADC698",
+        "dat-red": "#EB5160",
+        "dat-black": "#071013",
+        "dat-white": "#F8F8FF",
+      },
+    },
   },
   plugins: [],
 }
