db3238bb3627bb09bfc03e92dccfcdf4516068c5
artiecodes <bjarke.dehlie@gmail.com>
2023-12-14 15:22:59 +0100

    login.jsx done for now, add logo.svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
+import Login from './pages/auth/Login'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    < Login />
     </>
   )
 }
