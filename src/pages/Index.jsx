80e0090e829ed8aa5b6206ca6cfc69e3d2f57f81
Julius Lassen <julius5517@gmail.com>
2023-12-15 01:17:34 +0100

    update style index

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -2,18 +2,18 @@ import logo from "../assets/logo/logo.svg";
 
 function Index() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center">
 
       <img src={logo} alt="Blogged logo" className="w-1/6" />
 
-      <h1 className="text-2xl font-bold mb-4">Login or signup</h1>
+      <h1 className="text-4xl font-bold mb-4">Login or signup</h1>
       <p className="mb-4">
         Are you ready to be blogged?
       </p>
 
-      <button to="/login" className="bg-black text-white font-semibold py-2 px-16 rounded-full mb-2">Login</button>
-      <p className="mb-2">or</p>
-      <button to="/signup" className="bg-blue-500 text-white font-semibold py-2 px-16 rounded-full ">Signup</button>
+      <button to="/login" className="bg-black text-white font-semibold py-2 w-40 rounded-full mt-12">Login </button>
+      <p className="my-4">or</p>
+      <button to="/signup" className="bg-blue-500 text-white font-semibold py-2 w-40 rounded-full">Signup</button>
       
     </div>
   );
