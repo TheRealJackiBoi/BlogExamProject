2d501178d6f736bf48363bb4ccf961e52bb00ea2
artiecodes <bjarke.dehlie@gmail.com>
2023-12-15 14:42:04 +0100

    added password checker, added zxcvbn

--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import logo from "../../assets/logo.svg";
+import zxcvbn from "zxcvbn";
 import { Icon } from "react-icons-kit";
-import {withLine} from 'react-icons-kit/entypo/withLine'
-import {eye} from 'react-icons-kit/entypo/eye'
+import { withLine } from "react-icons-kit/entypo/withLine";
+import { eye } from "react-icons-kit/entypo/eye";
+
+// npm install react-icons-kit
+// npm install zxcvbn
 
 const SignUp = () => {
   const [username, setUsername] = useState("");
@@ -13,10 +17,10 @@ const SignUp = () => {
   // to have a big if statement rather than a big else statement
   const [passwordMatch, setPasswordMatch] = useState(true);
   const [formComplete, setFormComplete] = useState(false);
+  const [passwordStrength, setPasswordStrength] = useState(0);
   const [type, setType] = useState("password");
   const [icon, setIcon] = useState(withLine);
 
-  // npm install react-icons-kit
   const handleToggle = () => {
     if (type === "password") {
       setIcon(eye);
@@ -36,6 +40,37 @@ const SignUp = () => {
       setFormComplete(
         !!username && !!password && !!repeatPassword && !!securityQuestion
       );
+    }
+  };
+
+  const getPasswordStrengthColor = (score) => {
+    switch (score) {
+      case 0:
+      case 1:
+        return "text-dat-red";
+      case 2:
+        return "text-dat-yellow";
+      case 3:
+        return "text-dat-olive";
+      case 4:
+        return "text-dat-olive";
+      default:
+        return "text-dat-gray";
+    }
+  };
+  
+  const getPasswordStrengthLabel = (score) => {
+    switch (score) {
+      case 0:
+        return "Weak";
+      case 1:
+        return "Weak";
+      case 2:
+        return "Moderate";
+      case 3:
+        return "Strong";
+      default:
+        return "Very Strong";
     }
   };
 
@@ -70,55 +105,71 @@ const SignUp = () => {
             <label className="mb-2 grid grid-cols-1">
               <div className="text-left">Password</div>
               <div className="relative">
-              <input
-                className="bg-dat-white border border-dat-black p-2 rounded w-60 max-w-[calc(100%)2rem)] shadow-md shadow-gray-400 mb-2 pr-7"
-                type={type}
-                value={password}
-                placeholder="Password"
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-                onBlur={() =>
-                  setFormComplete(
-                    !!username &&
-                      !!password &&
-                      !!repeatPassword &&
-                      !!securityQuestion
-                  )
-                }
-              />
-              <span
-                class="absolute right-2 top-1.5 cursor-pointer"
-                onClick={handleToggle}>
-                <Icon icon={icon} size={20} />
-              </span>
+                <input
+                  className="bg-dat-white border border-dat-black p-2 rounded w-60 max-w-[calc(100%)2rem)] shadow-md shadow-gray-400 mb-2 pr-7"
+                  type={type}
+                  value={password}
+                  placeholder="Password"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                    const result = zxcvbn(e.target.value);
+                    console.log(result);
+                    const strength = result.score;
+                    console.log('Strength:', strength);
+                    setPasswordStrength(strength);
+                  }}
+                  
+                  onBlur={() =>
+                    setFormComplete(
+                      !!username &&
+                        !!password &&
+                        !!repeatPassword &&
+                        !!securityQuestion
+                    )
+                  }
+                />
+                <span
+                  className="absolute right-2 top-1.5 cursor-pointer"
+                  onClick={handleToggle}>
+                  <Icon icon={icon} size={20} />
+                </span>
+                {password && (
+                  <div className="mt-1 text-sm">
+                    Strength:{" "}
+                    <span
+                      className={`${getPasswordStrengthColor(
+                        passwordStrength
+                      )}`}>
+                      {getPasswordStrengthLabel(passwordStrength)}
+                    </span>
+                  </div>
+                )}
               </div>
             </label>
             <label className="mb-2 grid grid-cols-1">
               <div className="text-left">Repeat password</div>
               <div className="relative">
-              <input
-                className="bg-dat-white border border-dat-black p-2 rounded w-60 shadow-md shadow-gray-400 mb-2"
-                type={type}
-                value={repeatPassword}
-                placeholder="Repeat password"
-                onChange={(e) => {
-                  setRepeatPassword(e.target.value);
-                  setPasswordMatch(true);
-                }}
-                onBlur={() =>
-                  setFormComplete(
-                    !!username &&
-                      !!password &&
-                      !!repeatPassword &&
-                      !!securityQuestion
-                  )
-                }
-              />
-              <span
-                class="absolute right-2 top-2 cursor-pointer"
-                onClick={handleToggle}>
-              </span>
+                <input
+                  className="bg-dat-white border border-dat-black p-2 rounded w-60 shadow-md shadow-gray-400 mb-2"
+                  type={type}
+                  value={repeatPassword}
+                  placeholder="Repeat password"
+                  onChange={(e) => {
+                    setRepeatPassword(e.target.value);
+                    setPasswordMatch(true);
+                  }}
+                  onBlur={() =>
+                    setFormComplete(
+                      !!username &&
+                        !!password &&
+                        !!repeatPassword &&
+                        !!securityQuestion
+                    )
+                  }
+                />
+                <span
+                  className="absolute right-2 top-2 cursor-pointer"
+                  onClick={handleToggle}></span>
               </div>
               {!passwordMatch && (
                 <p className="text-dat-red text-sm">
