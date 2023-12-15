f24fbf8c20fda15e749b0d095dfbf6ff8cbd19b4
Julius Lassen <julius5517@gmail.com>
2023-12-15 14:14:04 +0100

    used dat-colors

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -11,9 +11,9 @@ function Index() {
         Are you ready to be blogged?
       </p>
 
-      <button to="/login" className="bg-black text-white font-semibold py-2 w-40 rounded-full mt-12">Login </button>
+      <button to="/login" className="bg-dat-black text-white font-semibold py-2 w-40 rounded-full mt-12">Login </button>
       <p className="my-4">or</p>
-      <button to="/signup" className="bg-blue-500 text-white font-semibold py-2 w-40 rounded-full">Signup</button>
+      <button to="/signup" className="bg-dat-blue text-white font-semibold py-2 w-40 rounded-full">Signup</button>
       
     </div>
   );
