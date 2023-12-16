d1b36950d29b7dade93a6c34ef33d71814ca9bb0
TheRealJackiBoi <jslam@oulund.dk>
2023-12-16 21:49:52 +0100

    add login, setToken, getToken and logout

---
+++ src/api/services/auth/auth.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import { BASE_URL } from './../config.js'
+
+
+// Get and Set token
+export const setToken = (token) => {
+
+    localStorage.setItem('token', token)
+}
+
+export const getToken = () => {
+
+    return localStorage.getItem('token')
+}
+
+// Logout
+export const logout = (callback) => {
+
+    localStorage.removeItem('token')
+    callback(false)
+}
+
+
+export const login = async (username, password, callback) => {
+
+    const data = {
+        username: username,
+        password: password
+    }
+
+    try {
+    const response = await axios.post(`${BASE_URL}/auth/login`, 
+        data, 
+        {
+            withCredentials: true,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+    const token = response.data
+    callback(true)
+    setToken(token)
+    } 
+    catch (error) {
+        console.error(error)
+    }
+}
+
+
