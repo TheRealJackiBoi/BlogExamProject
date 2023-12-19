2f57e5fc02711417495677647bec2bbcea6e7c5f
artiecodes <bjarke.dehlie@gmail.com>
2023-12-19 16:06:12 +0100

    homepage functionality WIP

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,95 +5,57 @@ import { useLoaderData } from "react-router-dom"
 
 const Home = () => {
   const posts = useLoaderData();
-
-
-  // Like functionality
   const [likeCount, setLikeCount] = useState(0);
 
   const handleLikeClick = () => {
     setLikeCount(likeCount + 1);
   };
 
+  // Convert posts to an array
+  const postsArray = Object.values(posts);
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-1/4 mx-auto">
+    <div className="flex flex-col items-center justify-center h-fit mt-10 w-1/4 mx-auto">
       <h1 className="mb-4 text-3xl font-bold">Blogged community says...</h1>
 
-      <div className="mb-4">
-        <section className="h-fit bg-dat-olive p-10 text-center relative">
-          <h3
-            className="mb-3 text-xl"
-            style={{ position: "relative", top: "-16px" }}>
-            Lorem Ipsum
-          </h3>
-          {/* Date */}
-          <div className="absolute top-16 text-xs text-gray-500">
-            18-12-2023
-          </div>
+      {postsArray.map((post, index) => (
+        <div key={index} className="mb-4">
+          <section
+            className="bg-dat-olive p-10 text-center relative"
+          >
+            <h3
+              className="mb-3 text-xl"
+              style={{ position: "relative", top: "-16px" }}
+            >
+              {post.title}
+            </h3>
+            {/* Date */}
+            <div className="absolute top-16 text-xs text-gray-500">
+              {post.createdAt}
+            </div>
 
-          {/* Read more */}
-          <div className="absolute bottom-4 right-10 text-xs text-gray-500 cursor-pointer">
-            Read more
-          </div>
+            {/* Read more */}
+            <div className="absolute bottom-4 right-10 text-xs text-gray-500 cursor-pointer">
+              Read more
+            </div>
 
-          {/* Like */}
-          <div
-            className="absolute bottom-4 text-gray-500 cursor-pointer"
-            onClick={handleLikeClick}>
-            <Icon icon={heart} size={14} />
-          </div>
-          <div className="absolute bottom-4 left-14 text-sm text-gray-500">
-            {likeCount}
-          </div>
-          {/* Input Content */}
-          <div className="bg-dat-white text-dat-black p-4 rounded shadow-md text-left relative">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec
-              sagittis aliquam malesuada bibendum arcu vitae elementum curabitur
-              vitae.
-            </p>
-          </div>
-        </section>
-      </div>
-
-      {/* Testing how it looks with a second post */}
-      <div className="mb-4">
-        <section className="h-fit bg-dat-olive p-10 text-center relative">
-          <h3
-            className="mb-3 text-xl"
-            style={{ position: "relative", top: "-16px" }}>
-            Lorem Ipsum
-          </h3>
-          {/* Date */}
-          <div className="absolute top-16 text-xs text-gray-500">
-            18-12-2023
-          </div>
-
-          {/* Read more */}
-          <div className="absolute bottom-4 right-10 text-xs text-gray-500 cursor-pointer">
-            Read more
-          </div>
-
-          {/* Like */}
-          <div
-            className="absolute bottom-4 text-gray-500 cursor-pointer"
-            onClick={handleLikeClick}>
-            <Icon icon={heart} size={14} />
-          </div>
-          <div className="absolute bottom-4 left-14 text-sm text-gray-500">
-            {likeCount}
-          </div>
-          {/* Input Content */}
-          <div className="bg-dat-white text-dat-black p-4 rounded shadow-md text-left relative">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec
-              sagittis aliquam malesuada bibendum arcu vitae elementum curabitur
-              vitae.
-            </p>
-          </div>
-        </section>
-      </div>
+            {/* Like */}
+            <div
+              className="absolute bottom-4 text-gray-500 cursor-pointer"
+              onClick={handleLikeClick}
+            >
+              <Icon icon={heart} size={14} />
+            </div>
+            <div className="absolute bottom-4 left-14 text-sm text-gray-500">
+              {likeCount}
+            </div>
+            {/* Input Content */}
+            <div className="bg-dat-white text-dat-black p-4 rounded shadow-md text-left relative">
+              <p>{post.content}</p>
+            </div>
+          </section>
+        </div>
+      ))}
     </div>
   );
 };
