a7c9a0ba36847696f94222513d97af5ad14dbe74
artiecodes <bjarke.dehlie@gmail.com>
2023-12-19 12:52:08 +0100

    functionality wip

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import { Icon } from "react-icons-kit";
 import { heart } from "react-icons-kit/icomoon/heart";
 
-const API_URL = "";
-
 const Home = () => {
   // Like functionality
   const [likeCount, setLikeCount] = useState(0);
@@ -12,21 +10,16 @@ const Home = () => {
     setLikeCount(likeCount + 1);
   };
 
-  // Loader
-  const postLoader = async () => {
-    const response = await fetch(API_URL);
-    const data = await response.json();
-    console.log(data);
-  }
-
   return (
     <div className="flex flex-col items-center justify-center h-screen w-1/4 mx-auto">
       <h1 className="mb-4 text-3xl font-bold">Blogged community says...</h1>
 
       <div className="mb-4">
         <section className="h-fit bg-dat-olive p-10 text-center relative">
-        <h3 className="mb-3 text-xl" style={{ position: "relative", top: "-16px" }}>
-        Lorem Ipsum
+          <h3
+            className="mb-3 text-xl"
+            style={{ position: "relative", top: "-16px" }}>
+            Lorem Ipsum
           </h3>
           {/* Date */}
           <div className="absolute top-16 text-xs text-gray-500">
@@ -62,8 +55,10 @@ const Home = () => {
       {/* Testing how it looks with a second post */}
       <div className="mb-4">
         <section className="h-fit bg-dat-olive p-10 text-center relative">
-        <h3 className="mb-3 text-xl" style={{ position: "relative", top: "-16px" }}>
-        Lorem Ipsum
+          <h3
+            className="mb-3 text-xl"
+            style={{ position: "relative", top: "-16px" }}>
+            Lorem Ipsum
           </h3>
           {/* Date */}
           <div className="absolute top-16 text-xs text-gray-500">
