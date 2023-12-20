3f9f0661f3b85d7025431fe12091ec70c3a5aa71
Julius Lassen <julius5517@gmail.com>
2023-12-20 18:00:50 +0100

    500 error added 🥰

--- src/api/services/posts.js
+++ src/api/services/posts.js
@@ -1,5 +1,9 @@
+import axios from 'axios';
+import { BASE_URL } from './config.js'
+import { getToken } from './auth/auth.js'
+
 export const createPost = async (username, title, content, visibility) => {
-    
+
     const data = {
         "username": username,
         "title": title,
@@ -14,9 +18,12 @@ export const createPost = async (username, title, content, visibility) => {
             withCredentials: true,
             headers: {
                 "Content-Type": "application/json",
+                'Accept': 'application/json',
+                Authorization: `Bearer ${getToken()}`,
             },
         })
-    
+        console.log('Post created:', response.data);
+
     } 
     catch (error) {
         console.error(error)
