ab198aef6d68d49f455f6ec12a2c716837b24758
artiecodes <bjarke.dehlie@gmail.com>
2023-12-21 14:45:22 +0100

    small fix

--- src/components/post/Like.jsx
+++ src/components/post/Like.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Icon } from 'react-icons-kit';
-import { heart } from 'react-icons-kit/icomoon/heart';
-import { handleLikeClick } from '../../api/services/posts';
+import React from "react";
+import { Icon } from "react-icons-kit";
+import { heart } from "react-icons-kit/icomoon/heart";
+import { handleLikeClick } from "../../api/services/posts";
 
 const Like = ({ postId, likes, updateLikes }) => {
   return (
@@ -9,8 +9,7 @@ const Like = ({ postId, likes, updateLikes }) => {
       {/* Like */}
       <div
         className="absolute bottom-4 text-gray-500 cursor-pointer"
-        onClick={() => handleLikeClick(postId, likes, updateLikes)}
-      >
+        onClick={() => handleLikeClick(postId, likes, updateLikes)}>
         <Icon icon={heart} size={14} />
       </div>
       <div className="absolute bottom-4 left-14 text-sm text-gray-500">
