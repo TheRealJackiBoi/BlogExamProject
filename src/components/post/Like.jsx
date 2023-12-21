62ba0da05f9b7ffafd08469ba1e4b563e6c0fe93
artiecodes <bjarke.dehlie@gmail.com>
2023-12-21 16:12:07 +0100

    like wip, heart colour change

--- src/components/post/Like.jsx
+++ src/components/post/Like.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Icon } from "react-icons-kit";
 import { heart } from "react-icons-kit/icomoon/heart";
 import { handleLikeClick } from "../../api/services/posts";
 
 const Like = ({ postId, likes, updateLikes }) => {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleClick = async () => {
+    try {
+      await handleLikeClick(postId, likes, updateLikes);
+      setIsLiked(true);
+    } catch (error) {
+      console.error("Error updating likes:", error);
+    }
+  };
+
   return (
     <>
-      {/* Like */}
       <div
-        className="absolute bottom-4 text-gray-500 cursor-pointer"
-        onClick={() => handleLikeClick(postId, likes, updateLikes)}>
+        className={`absolute bottom-4 text-${
+          isLiked ? "dat-red" : "gray"
+        }-500 cursor-pointer`}
+        onClick={handleClick}>
         <Icon icon={heart} size={14} />
       </div>
       <div className="absolute bottom-4 left-14 text-sm text-gray-500">
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -34,25 +34,21 @@ const Post = () => {
             <div className="absolute top-16 text-xs text-gray-500">
               {formatDateString(post.createdAt.slice(0, 10))}
             </div>
-
             {/* Read more */}
             <div className="absolute bottom-4 right-10 text-xs text-gray-500 cursor-pointer">
               Read more
             </div>
-
             {/* User
             maybe add a link to their userprofile on click? */}
             <div className="absolute top-16 right-10 text-xs text-gray-500">
               {post.username}
             </div>
-
             {/* Like */}
             <Like
               postId={post.id}
               likes={post.likes}
               updateLikes={updateLikes}
             />
-
             {/* Input Content */}
             <div className="bg-dat-white text-dat-black p-4 rounded shadow-md text-left relative">
               <p>{post.content}</p>
