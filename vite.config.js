d9ca51da90e8140d07a436ddf06f107ee01f18a0
TheRealJackiBoi <jslam@oulund.dk>
2023-12-30 23:37:33 +0100

    add env vars to config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    VITE_SUPAKEY: process.env.VITE_SUPAKEY,
+    VITE_DEPLOYED: process.env.VITE_DEPLOYED,
+  }
 })
