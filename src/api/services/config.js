9f4e1e91ffa6b094e7fe398633e28f47c205a9d2
TheRealJackiBoi <jslam@oulund.dk>
2024-01-02 00:52:53 +0100

    update url

--- src/api/services/config.js
+++ src/api/services/config.js
@@ -1,2 +1,1 @@
-
-export const BASE_URL = 'http://localhost:7070/api'//!import.meta.env.VITE_DEPLOYED ? 'http://localhost:7070/api' : 'https://api.localhost.com/api';
+export const BASE_URL = !import.meta.env.VITE_DEPLOYED ? 'http://localhost:7070/api' : 'https://bloggedapi.cudia.dk/api';
