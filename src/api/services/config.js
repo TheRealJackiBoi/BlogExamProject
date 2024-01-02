b2c176e78614ee20bacf1651efece90b8841d216
Jack <33868006+TheRealJackiBoi@users.noreply.github.com>
2024-01-02 03:01:51 +0100

    Update config.js

--- src/api/services/config.js
+++ src/api/services/config.js
@@ -1,1 +1,1 @@
-export const BASE_URL = !import.meta.env.VITE_DEPLOYED ? 'http://localhost:7070/api' : 'bloggedapi.cudia.dk:7070/api';
+export const BASE_URL = !import.meta.env.VITE_DEPLOYED ? 'http://localhost:7070/api' : 'http://bloggedapi.cudia.dk:7070/api';
