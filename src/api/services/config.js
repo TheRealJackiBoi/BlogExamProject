7d5fb4869b5378570e0f74ebf37b53cf1cf61fc7
artiecodes <bjarke.dehlie@gmail.com>
2024-01-03 13:38:51 +0100

    fix config

--- src/api/services/config.js
+++ src/api/services/config.js
@@ -1,1 +1,9 @@
-export const BASE_URL = !import.meta.env.VITE_DEPLOYED ? 'http://localhost:7070/api' : 'http://bloggedapi.cudia.dk:7070/api';
+let BASE_URL
+if (import.meta.env.VITE_DEPLOYED === true) {
+    BASE_URL = 'http://bloggedapi.cudia.dk:7070/api'
+} else {
+    BASE_URL = 'http://localhost:7070/api'
+}
+// export const BASE_URL = !import.meta.env.VITE_DEPLOYED ? 'http://localhost:7070/api' : 'http://bloggedapi.cudia.dk:7070/api';
+
+export { BASE_URL }
