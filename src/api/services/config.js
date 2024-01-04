fad17ad0619845b206453e9daa18cb690776cfaa
Jack <33868006+TheRealJackiBoi@users.noreply.github.com>
2024-01-04 03:23:07 +0100

    Update config.js

--- src/api/services/config.js
+++ src/api/services/config.js
@@ -1,5 +1,9 @@
 let BASE_URL
-if (import.meta.env.VITE_DEPLOYED == true) {
+const DEPLOYED = import.meta.env.VITE_DEPLOYED
+
+console.log("dep", DEPLOYED)
+
+if (DEPLOYED == true) {
     BASE_URL = 'http://bloggedapi.cudia.dk:7070/api'
 } else {
     BASE_URL = 'http://localhost:7070/api'
