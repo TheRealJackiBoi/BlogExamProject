bee3126e41b204880b0af9eff7d5fcf18d2302d4
artiecodes <bjarke.dehlie@gmail.com>
2024-01-08 14:01:17 +0100

    readmore func

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,54 +1,34 @@
-<<<<<<< HEAD
-import React from "react";
-import { getUsername } from "../../api/services/auth/auth";
-import Like from "./Like";
-import Edit from "./Edit";
-import Username from "./Username";
-import ReadMore from "./ReadMore";
-=======
+import { useState } from "react";
 import PostInformation from "./PostInformation";
 import PostActions from "./PostActions";
->>>>>>> 7202a8433800d581fb4028c02b38117bdc3309a1
 
 const Post = ({ post, handleLikeClickUpdate }) => {
-  
-  
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleReadMore = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <section className="bg-dat-olive z-0 p-10 text-center relative w-full min-w-fit my-4">
-      
-      <h3 className="mb-2 text-xl min-w-[250px]">
-        {post.title}
-      </h3>
-<<<<<<< HEAD
-      {/* Date */}
-      <div className="absolute top-16 text-xs text-gray-500">
-        {formatDateString(post.createdAt.slice(0, 10))}
-      </div>
-      {/* ReadMore */}
-      <ReadMore
-        postId={post.id}
-        handleLikeClickUpdate={handleLikeClickUpdate}
-      />
-      <Edit post={post} username={username} />
-      <Username username={post.username} />
-      {/* Like */}
-      <Like
-        postId={post.id}
-        likes={post.likes}
-        handleLikeClickUpdate={handleLikeClickUpdate}
-      />
-=======
-      
-      <PostInformation date={post.createdAt} username={post.username} />
-         
+      <h3 className="mb-2 text-xl min-w-[250px]">{post.title}</h3>
 
->>>>>>> 7202a8433800d581fb4028c02b38117bdc3309a1
+      <PostInformation date={post.createdAt} username={post.username} />
+
       {/* Input Content */}
-      <div className="bg-dat-white min-w-full text-dat-black p-4 rounded shadow-md text-left relative">
+      <div
+        className={`bg-dat-white min-w-full text-dat-black p-4 rounded shadow-md text-left relative ${
+          isExpanded ? "max-h-full" : "max-h-[100px]"
+        } overflow-hidden transition-max-h ease-in-out duration-300`}>
         <p>{post.content}</p>
       </div>
 
-      <PostActions post={post} handleLikeClickUpdate={handleLikeClickUpdate} />
+      <PostActions
+        post={post}
+        handleLikeClickUpdate={handleLikeClickUpdate}
+        toggleReadMore={toggleReadMore}
+        isExpanded={isExpanded}
+      />
     </section>
   );
 };
--- src/components/post/PostActions.jsx
+++ src/components/post/PostActions.jsx
@@ -1,25 +1,34 @@
-import Like from "./Like"
-import Edit from "./Edit"
+import React from "react";
+import Like from "./Like";
+import Edit from "./Edit";
+import ReadMore from "./ReadMore";
 
-const PostActions = ({ post, username, handleLikeClickUpdate }) => {
+const PostActions = ({
+  post,
+  username,
+  handleLikeClickUpdate,
+  toggleReadMore,
+  isExpanded,
+}) => {
+  return (
+    <div className="flex justify-between my-1">
+      <Like
+        postId={post.id}
+        likes={post.likes}
+        handleLikeClickUpdate={handleLikeClickUpdate}
+      />
 
-    return (
-        <div className="flex justify-between my-1">
-            <Like
-                postId={post.id}
-                likes={post.likes}
-                handleLikeClickUpdate={handleLikeClickUpdate}
-            />
+      <div className="flex-inline justify-between">
+        <Edit post={post} username={username} />
 
-            <div className="flex-inline justify-between">
-                <Edit post={post} username={username}/>
+        <ReadMore
+          onToggleReadMore={toggleReadMore}
+          isExpanded={isExpanded}
+          contentLength={post.content.length}
+        />
+      </div>
+    </div>
+  );
+};
 
-                <div className="text-xs text-gray-500 cursor-pointer">
-                    Read more
-                </div>
-            </div>
-        </div>
-    )
-}
-
-export default PostActions
+export default PostActions;
